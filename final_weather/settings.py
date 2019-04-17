59dfe66ad58341510d094956ee68c18b60d183d2
Ubuntu <ubuntu@ip-172-31-85-229.ec2.internal>
2019-04-17 05:17:33 +0000

    Aws Changes

--- final_weather/settings.py
+++ final_weather/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'd$dt6($!q5a=l2_3_#v_z5ts89j(gamwxhp=*544!_skmy6&v7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['server_domain_or_IP','3.87.184.48']
 
 
 # Application definition
@@ -130,3 +130,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
